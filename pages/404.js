a688d594a433946d1c2059a84db486b4da1d5216
kodgabriel kodgabriel <gabriel2793@yahoo.com.br>
2024-03-01 15:53:20 -0300

    primeiro curso de next js alura

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+export default function Pagina404() {
+    return (
+        <div>
+            <h1>Você se perdeu, volte para a Home Page</h1>
+            <Link href="/">
+                Ir para a Home Page
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "../src/components/Link";
+import HomeScreen from "../src/screens/HomeScreen";
+
+function Title({ children, as }) {
+    const Tag = as;
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx> {`
+                ${Tag} {
+                    color: red;                    
+                }
+            `}
+            </style>
+        </React.Fragment>
+    )
+}
+
+export default function HomePage() {
+    return (
+        <div>
+            {/* <Title as="h1">Alura Cases - Home Page</Title>
+            <Link href="/faq">
+                Ir para a página do FAQ
+            </Link> */}
+            <HomeScreen />
+        </div>
+    )
+}
